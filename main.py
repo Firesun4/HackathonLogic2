1555f53f97f778f1f9fb19f32071db900527e4ef
Pranav Sarma <pranavsarma4@yahoo.com>
2023-02-18 10:24:20 -0600

    added requirements file

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 from flask import Flask, render_template, request
-from importlib.metadata import metadata
-from xmlrpc.client import boolean
-# from fastapi import FastAPI, Depends, Form
-# from fastapi.middleware.cors import CORSMiddleware
-# from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy import update
-from sql import SessionLocal, engine, meta
-from models import PollutionMapData
 import sys
-
-
 sys.path.append(".")
 
 app = Flask(__name__)
@@ -38,12 +26,7 @@ def update_Data():
     pass
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+
 
 if __name__ == "__main__":
     app.run()
