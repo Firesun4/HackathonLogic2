1087d02283cf023cea983f825fcc339c0f307c35
chaviw <cweingarten67@gmail.com>
2023-02-18 14:22:03 -0600

    Add files via upload

---
+++ justic_map_overlay.js
@@ -0,0 +1,61 @@
+  let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 30.267230, lng: -97.751978 },
+    zoom: 10,
+  });
+
+const moonMapType = new google.maps.ImageMapType({
+    getTileUrl: function (coord, zoom) {
+      const normalizedCoord = getNormalizedCoord(coord, zoom);
+
+      if (!normalizedCoord) {
+        return "";
+      }
+
+      return (
+        "http://www.justicemap.org/tile/block/plural" +
+        "/" +
+        zoom +
+        "/" +
+        normalizedCoord.x +
+        "/" +
+        normalizedCoord.y +
+        ".png"
+      );
+    },
+    tileSize: new google.maps.Size(256, 256),
+    maxZoom: 9,
+    minZoom: 0,
+    // @ts-ignore TODO 'radius' does not exist in type 'ImageMapTypeOptions'
+    radius: 1738000,
+    name: "JusticeMap",
+  });
+
+  map.mapTypes.set("justicemap", moonMapType);
+  map.setMapTypeId("justicemap");
+}
+
+// Normalizes the coords that tiles repeat across the x axis (horizontally)
+// like the standard Google map tiles.
+function getNormalizedCoord(coord, zoom) {
+  const y = coord.y;
+  let x = coord.x;
+  // tile range in one direction range is dependent on zoom level
+  // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
+  const tileRange = 1 << zoom;
+
+  // don't repeat across y-axis (vertically)
+  if (y < 0 || y >= tileRange) {
+    return null;
+  }
+
+  // repeat across x-axis
+  if (x < 0 || x >= tileRange) {
+    x = ((x % tileRange) + tileRange) % tileRange;
+  }
+  return { x: x, y: y };
+}
+
+window.initMap = initMap;
