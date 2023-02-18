6b88cedbd8da1d5d89296902852edd9f6c4bc15b
Adam <adamdtaylor2005@gmail.com>
2023-02-18 14:38:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -123,6 +123,29 @@ window.onload = function () {
     }
     <div id="map_canvas" style="height: 500px; width: 100%;"></div>
 };
+
+async function getAllData() {
+  const response = await fetch('http://localhost:8000/get/all-aqi')
+  const the_response = await(response.json())
+    return the_response;
+}
+
+
+async function getDataField(field) {
+  response = await(getAllData())
+  console.log(field)
+  var listOfDicts = []
+  for(let i = 0; i < response.length - 1; i++){
+    var dict = {
+      lat: response[i].latitude,
+      long: response[i].longitude,
+    }
+    dict[field] = response[i][field]
+    listOfDicts.push(dict)
+  }
+  console.log(listOfDicts)
+  return dict
+}
 </script>
 //k
 
