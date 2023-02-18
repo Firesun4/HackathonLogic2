9ba7cce71819c8470a28f4de8c55045c6045e263
Adam <adamdtaylor2005@gmail.com>
2023-02-18 09:57:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template, request
+from importlib.metadata import metadata
+from xmlrpc.client import boolean
+# from fastapi import FastAPI, Depends, Form
+# from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import update
+from sql import SessionLocal, engine, meta
+from models import PollutionMapData
 import sys
+
+
 sys.path.append(".")
 
 app = Flask(__name__)
@@ -21,7 +33,12 @@ def get_bot_response():
 # function to update database'
 def update_Data():
 
-
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 if __name__ == "__main__":
     app.run()
